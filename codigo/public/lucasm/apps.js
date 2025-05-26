eb3758818dc993855b7ec3833933cefed6c515dd
lucas <lucasmaiaxgo@gmail.com>
2025-05-26 15:34:38 -0300

    apps.js (mascara cpf)
    usando imask pra fazer o cpf (e os outros dados)

---
+++ codigo/public/lucasm/apps.js
@@ -0,0 +1,9 @@
+const cpfInput = document.getElementById('cpf');
+const telefoneInput = document.getElementById('telefone');
+const cepInput = document.getElementById('cep');
+
+IMask(cpfInput, {
+  mask: '000.000.000-00'
+});
+
+
